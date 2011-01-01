0d8451e0c10a93430fb69c59ae7bd8796691e65f
sorrycc <sorrycc@gmail.com>
2011-01-01 15:42:04 +0800

    remote make.sh

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SPHINXBUILD=sphinx-build
-BUILDDIR=docs
-ALLSPHINXOPTS="-d ${BUILDDIR}/doctrees ${SPHINXOPTS} source"
-
-echo "sphinx-build -b html ${ALLSPHINXOPTS} ${BUILDDIR}/html"
-echo "Build finished. The HTML pages are in %BUILDDIR%/html."
