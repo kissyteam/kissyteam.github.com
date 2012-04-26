3797e1bb68878591ce096f9f35889a921e4c1fb2
yiminghe <yiminghe@gmail.com>
2012-04-26 14:55:07 +0800

    remove countdown

--- docs/html/static/demo/switchable/assets/demo1.js
+++ docs/html/static/demo/switchable/assets/demo1.js
@@ -1,9 +1,7 @@
 KISSY.use("switchable", function (S, Switchable) {
     var s = new Switchable.Slide('#J_Slide', {
         effect : 'scrolly',
-        easing : 'easeOutStrong',
-        countdown : true,
-        countdownFromStyle : 'width:18px'
+        easing : 'easeOutStrong'
     });
  });
  
