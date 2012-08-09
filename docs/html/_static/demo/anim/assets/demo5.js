f522446b6d8153a184bbf221c90b217ea93b3866
yiminghe <yiminghe@gmail.com>
2012-08-09 10:39:12 +0800

    rebuild

---
+++ docs/html/_static/demo/anim/assets/demo5.js
@@ -0,0 +1,43 @@
+KISSY.use("anim,node,button", function(S, Anim, Node, Button) {
+    var $ = Node.all;
+
+    var go = new Button({
+        content: "GO!"
+    }), stop = new Button({
+        content: "STOP!"
+    }), back = new Button({
+        content: "BACK!"
+    });
+    go.render();
+    stop.render();
+    back.render();
+
+    /* Start animation */
+    go.on("click", function(e) {
+        go.set("disabled", true);
+        back.set("disabled", true);
+        $(".block").animate({left: (parseInt($(".block").css("left")) + 100) + 'px'}, 2, undefined, function() {
+            go.set("disabled", false);
+            back.set("disabled", false);
+        });
+        e.halt();
+    });
+
+    /* Stop animation when button is clicked */
+    stop.on('click', function() {
+        go.set("disabled", false);
+        back.set("disabled", false);
+        $(".block").stop();
+    });
+
+    /* Start animation in the opposite direction */
+    back.on('click', function(e) {
+        go.set("disabled", true);
+        back.set("disabled", true);
+        $(".block").animate({left: (parseInt($(".block").css("left")) - 100) + 'px'}, 2, undefined, function() {
+            go.set("disabled", false);
+            back.set("disabled", false);
+        });
+        e.halt();
+    });
+});
---
+++ docs/html/_static/demo/editor/assets/auto-combo.js
@@ -0,0 +1,43 @@
+(function () {
+    KISSY.config("combine", true);
+    KISSY.use("editor", function (S, Editor) {
+
+        var cfg = {
+            // 是否初始聚焦
+            focused:true,
+            autoRender:true,
+            attachForm:true,
+            // 自定义样式
+            // customStyle:"p{line-height: 1.4;margin: 1.12em 0;padding: 0;}",
+            // 自定义外部样式
+            // customLink:["http://localhost/customLink.css","http://xx.com/y2.css"],
+            // render:"#container",
+            render:'#editorContainer',
+            width:'80%',
+            height:"400px"
+        };
+
+        KISSY.use("editor/plugin/source-area/," +
+            "editor/plugin/font-size/," +
+            "editor/plugin/image/", function (S, SourceArea, FontSize, Image) {
+
+            cfg.plugins = [SourceArea, FontSize, new Image({
+                upload:{
+                    serverUrl:"http://localhost/kissy_git/kissy/src/editor/demo/upload.php",
+                    serverParams:{
+                        waterMark:function () {
+                            return S.one("#ke_img_up_watermark_1")[0].checked;
+                        }
+                    },
+                    suffix:"png,jpg,jpeg,gif",
+                    fileInput:"Filedata",
+                    sizeLimit:1000, //k
+                    extraHtml:"<p style='margin-top:10px;'><input type='checkbox' id='ke_img_up_watermark_1' checked='checked'> 图片加水印，防止别人盗用</p>"
+                }
+            })];
+
+            new Editor(cfg);
+        });
+
+    });
+})();
