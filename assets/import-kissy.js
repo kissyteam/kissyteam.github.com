0bc4cda49c8b39b4a5df20c7fbb4676112493c5a
yiminghe <yiminghe@gmail.com>
2012-10-29 23:23:45 +0800

    update mousewheel

--- assets/import-kissy.js
+++ assets/import-kissy.js
@@ -3,6 +3,9 @@
     if (h.indexOf("localhost/kissy_git/") != -1) {
         d.write('<script src="http://localhost/kissy_git/kissy/build/kissy.js"' + ' charset="utf-8"><' + '/script>');
     }
+    else if (h.indexOf("chengyu.taobao.ali.com/kissy_git/") != -1) {
+        d.write('<script src="http://chengyu.taobao.ali.com/kissy_git/kissy/build/kissy.js"' + ' charset="utf-8"><' + '/script>');
+    }
     else if (h.indexOf("fed.ued.taobao.net/kissy-team/kissyteam/") != -1) {
         d.write('<script src="http://fed.ued.taobao.net/kissy-team/kissy/build/kissy.js"' + ' charset="utf-8">' + '<' + '/script>');
     }
