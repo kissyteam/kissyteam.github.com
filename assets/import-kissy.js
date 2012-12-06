c0a48488dafdc87ba6b40ef27ccfca7b2e2351d3
yiminghe <yiminghe@gmail.com>
2012-12-06 21:49:08 +0800

    up

--- assets/import-kissy.js
+++ assets/import-kissy.js
@@ -4,26 +4,27 @@
     if (h.indexOf("localhost/kissy_git/") != -1) {
         d.write('<script src="http://localhost/kissy_git/kissy/build/kissy.js"' +
             ' charset="utf-8"><' + '/script>');
-
+// ie write 行内脚本立即执行
+        //d.write('<script>console.log("1:"+(+new Date()-now));</script>')
+        d.write('<script src="http://localhost/kissy_git/kissyteam/assets/import-gallery.js"' +
+            ' charset="utf-8"><' + '/script>');
     }
     else if (h.indexOf("chengyu.taobao.ali.com/kissy_git/") != -1) {
         d.write('<script src="http://chengyu.taobao.ali.com/kissy_git/kissy/build/kissy.js"' +
             ' charset="utf-8"><' + '/script>');
-
+        d.write('<script src="http://chengyu.taobao.ali.com/kissy_git/kissyteam/assets/import-gallery.js"' +
+            ' charset="utf-8"><' + '/script>');
     }
     else if (h.indexOf("fed.ued.taobao.net/kissy-team/kissyteam/") != -1) {
         d.write('<script src="http://fed.ued.taobao.net/kissy-team/kissy/build/kissy.js"' +
             ' charset="utf-8">' + '<' + '/script>');
-
+        d.write('<script src="http://fed.ued.taobao.net/kissy-team/kissyteam/assets/import-gallery.js"' +
+            ' charset="utf-8"><' + '/script>');
     }
     else {
         d.write('<script src="http://docs.kissyui.com/kissy/build/kissy.js"' +
             ' charset="utf-8">' + '<' + '/script>');
-
+        d.write('<script src="http://docs.kissyui.com/assets/import-gallery.js"' +
+            ' charset="utf-8"><' + '/script>');
     }
-    // ie write 行内脚本立即执行
-    //d.write('<script>console.log("1:"+(+new Date()-now));</script>')
-    d.write('<script src="http://docs.kissyui.com/assets/import-gallery.js"' +
-        ' charset="utf-8"><' + '/script>');
-
 })();
