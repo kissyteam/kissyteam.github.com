8219a9fd2dbefdb51455061b75813cf1304faf9f
yiminghe <yiminghe@gmail.com>
2013-01-09 04:13:47 +0800

    up

--- source/raw/demo/menu/assets/decorate.js
+++ source/raw/demo/menu/assets/decorate.js
@@ -1,7 +1,6 @@
-
-    S.use("menu", function(S, Menu) {
-        new Menu({
-            srcNode:"#menuTest",
-            prefixCls:"my-"
-        }).render();
-    });
+KISSY.use("menu", function (S, Menu) {
+    new Menu({
+        srcNode: "#menuTest",
+        prefixCls: "my-"
+    }).render();
+});
