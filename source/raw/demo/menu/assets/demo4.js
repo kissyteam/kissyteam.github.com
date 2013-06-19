ddc6311670bcca0563bc03167d73c6dc95cea557
yiminghe <yiminghe@gmail.com>
2013-06-19 23:01:17 +0800

    update

--- source/raw/demo/menu/assets/demo4.js
+++ source/raw/demo/menu/assets/demo4.js
@@ -3,25 +3,21 @@ KISSY.use("menu", function (S, Menu) {
     var m1 = new Menu.Item({
 
         selectable: true,
-        pinyin: "nx",
         content: "女鞋"
     }),
         m2 = new Menu.Item({
 
         selectable: true,
-        pinyin: "jjfw",
         content: "家居服务"
     }),
         m3 = new Menu.Item({
 
         selectable: true,
-        pinyin: "sj",
         content: "手机"
     }),
         m4 = new Menu.Item({
 
         selectable: true,
-        pinyin: "nznf",
         content: "女装女服"
     });
     // 过滤菜单
@@ -38,52 +34,6 @@ KISSY.use("menu", function (S, Menu) {
     filterMenu.addChild(m2);
     filterMenu.addChild(m3);
     filterMenu.addChild(m4);
-
-    var origFilterItems = filterMenu.filterItems;
-
-    /**
-     * 自定义过滤策略，不自定义则是简单的内容 indexOf 查找
-     * @param str
-     */
-    filterMenu.filterItems = function (str) {
-        var self = this,
-            view = self.get("view"),
-            _labelEl = view.get("labelEl"),
-            filterInput = view.get("filterInput");
-
-        // 有过滤条件提示隐藏，否则提示显示
-        _labelEl[str ? "hide" : "show"]();
-
-        var children = self.get("children");
-
-        // 拼音
-        if (/^[a-z\s]+$/i.test(str)) {
-
-            // 过滤所有子组件
-            S.each(children, function (c) {
-                var pinyin = c.get("pinyin"),
-                    content = c.get("content");
-
-                var index;
-                if ((index = pinyin.indexOf(str)) > -1) {
-                    // 如果符合过滤项
-                    // 显示
-                    c.show();
-                    content = content.substring(0, index) +
-
-                    "<span class='" + "ks-menuitem-hit" + "'>" + content.substring(index, index + str.length) + "<" + "/span>" + content.substring(index + str.length, content.length);
-
-                    // 匹配子串着重 wrap
-                    c.get("el").html(content);
-                } else {
-                    c.hide();
-                }
-            });
-
-        } else {
-            origFilterItems.call(this, str);
-        }
-    };
 
     // 渲染到页面中
     filterMenu.render();
