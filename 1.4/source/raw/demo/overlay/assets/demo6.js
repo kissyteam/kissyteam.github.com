c0e1afad6be22a59ee4572af3568b206d7e006d2
yiminghe <yiminghe@gmail.com>
2013-12-05 14:16:34 +0800

    update

--- 1.4/source/raw/demo/overlay/assets/demo6.js
+++ 1.4/source/raw/demo/overlay/assets/demo6.js
@@ -4,7 +4,7 @@ KISSY.use("node,overlay,button", function (S, Node, O, B) {
         headerContent: '收藏',
         closable:true,
         bodyContent: '<iframe scrolling="no" height="200" width="398" frameborder="0" name="popupIframe" src="http://favorite.taobao.com/popup/add_collection.htm?id=10166801469&amp;itemtype=1&amp;scjjc=1&amp;_tb_token_=3b38ed73a7775&amp;t=1305543755536"></iframe>',
-        //mask: true,
+        mask: true,
         align: {
             points: ['cc', 'cc']
         }
